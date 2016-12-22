65a2a7e3da85c7143819da2a19b9f7eb552443d9
mtellesy <m.al-tellesy@uot.edu.ly>
2016-12-22 14:02:53 +0200

    No

--- XamarinSQLite.Android/MainActivity.cs
+++ XamarinSQLite.Android/MainActivity.cs
@@ -23,7 +23,7 @@ namespace XamarinSQLite.Android
 
         private string GetDbPath()
         {
-            string dbname = "BCLV1.db";
+            string dbname = "BCLV4.db";
             string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             return Path.Combine(documentsPath, dbname);
         }
